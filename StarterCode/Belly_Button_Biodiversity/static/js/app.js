7ce065f43ee726ba07441fd0b1e1024b2d64190b
Robert <rperron91@gmail.com>
2019-05-04 18:44:23 -0400

    Really Stumped
    Not even close to heroku ready yet, but not from lack of trying. Stumped trying to build even the bubble plot or get any of the variables to display in the console.log... keep getting net::ERR_FILE_NOT_FOUND even when I think I have built the paths correctly

---
+++ StarterCode/Belly_Button_Biodiversity/static/js/app.js
@@ -0,0 +1,80 @@
+function buildMetadata(sample) {
+
+  // @TODO: Complete the following function that builds the metadata panel
+
+  // Use `d3.json` to fetch the metadata for a sample
+  // d3.json("/metadata/<sample>")
+    // Use d3 to select the panel with id of `#sample-metadata`
+  
+
+    // Use `.html("") to clear any existing metadata
+
+    // Use `Object.entries` to add each key and value pair to the panel
+    // Hint: Inside the loop, you will need to use d3 to append new
+    // tags for each key-value in the metadata.
+
+    // BONUS: Build the Gauge Chart
+    // buildGauge(data.WFREQ);
+};
+
+
+function buildCharts(sample) {
+
+  // @TODO: Use `d3.json` to fetch the sample data for the plots
+  d3.json("/samples/<sample>").then(sample)
+  console.log(sample)
+  
+  
+    // @TODO: Build a Bubble Chart using the sample data
+    var trace1 = {
+      x: data.otu_ids,
+      y: data.sample_values,
+      text: data.otu_labels,
+      mode: 'markers',
+      marker: {
+        color: ,
+        size: 
+      }
+    };
+    
+    var Data = [trace1];
+    
+    var layout = {
+      title: 'Belly Button Bacteria',
+      showlegend: false,
+      height: 600,
+      width: 600
+    };
+    Plotly.newPlot('bubble', Data, layout);
+    // @TODO: Build a Pie Chart
+    // HINT: You will need to use slice() to grab the top 10 sample_values,
+    // otu_ids, and labels (10 each).
+    
+function init() {
+  // Grab a reference to the dropdown select element
+  var selector = d3.select("#selDataset");
+
+  // Use the list of sample names to populate the select options
+  d3.json("/names").then((sampleNames) => {
+    sampleNames.forEach((sample) => {
+      selector
+        .append("option")
+        .text(sample)
+        .property("value", sample);
+    });
+
+    // Use the first sample from the list to build the initial plots
+    const firstSample = sampleNames[0];
+    buildCharts(firstSample);
+    buildMetadata(firstSample);
+  });
+};
+
+function optionChanged(newSample) {
+  // Fetch new data each time a new sample is selected
+  buildCharts(newSample);
+  buildMetadata(newSample);
+};
+
+// Initialize the dashboard
+init();
